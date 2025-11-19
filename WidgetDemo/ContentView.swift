21486a16e737ea60558ce53f33a236f7ef659ac2
superkookai <weerawut@live.com>
2025-11-19 14:06:15 +0700

    Initial Commit

---
+++ WidgetDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetDemo
+//
+//  Created by Weerawut Chaiyasomboon on 19/11/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by Weerawut Chaiyasomboon on 19/11/2568.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
